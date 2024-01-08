b0c0c8f1a6e5ac0a70191b1bf77fadc4415b4e0d
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2024-01-08 16:48:22 +0200

    version 8 working mail

--- contact_form.php
+++ contact_form.php
@@ -12,25 +12,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $topic = $_POST['topic'];
     $message = $_POST['message'];
 
-    // Instantiation and passing `true` enables exceptions
     $mail = new PHPMailer(true);
 
     try {
         // Server settings
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host       = 'smtp.gmail.com';               // Specify your SMTP server
-        $mail->SMTPAuth   = true;                             // Enable SMTP authentication
-        $mail->Username   = 'yourmail@gmail.com';      // SMTP username
-        $mail->Password   = 'ytth ijcv dibe zzes';                  // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                              // TCP port to connect to, use 587 for TLS
+        $mail->isSMTP();                            
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;  
+        $mail->Username   = 'bogdan.tyb9@gmail.com';
+        $mail->Password   = 'ytth ijcv dibe zzes';
+        $mail->SMTPSecure = 'tls';                            
+        $mail->Port       = 587;
 
         // Recipients
         $mail->setFrom($email);
         $mail->addAddress('bogdan.tyb9@gmail.com');
 
         // Content
-        $mail->isHTML(false);  // Set email format to plain text
+        $mail->isHTML(false);
         $mail->Subject = $topic;
         $mail->Body    = "Ім'я: $name\nЕл.пошта: $email\nНомер телефону: $number\nАдреса: $address\nТема: $topic\nПовідомлення: $message";
 
