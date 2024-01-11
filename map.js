ebfd59a1915c4c7843d007b38ccfd0170864dbf5
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2024-01-11 18:26:40 +0200

    test version

--- map.js
+++ map.js
@@ -1,10 +1,10 @@
 var map;
 
 function initMap() {
-    map = L.map('map').setView([49.65260985622448, 23.852961768567724], 10);
+    map = L.map('map').setView([49.83508248399536, 24.008341055404156], 10);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {grayscale: true}).addTo(map);
 
-    var marker = L.marker([49.65260985622448, 23.852961768567724]).addTo(map);
+    var marker = L.marker([49.83508248399536, 24.008341055404156]).addTo(map);
 
 }
 
